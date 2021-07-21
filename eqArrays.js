aed91ac8b59d16546970d89290ba4b5caf2a5366
Brian Sohn <kolpp15@gmail.com>
2021-07-21 14:03:37 -0700

    reuploading

--- eqArrays.js
+++ eqArrays.js
@@ -11,21 +11,19 @@ const assertEqual = function(actual, expected) {
 // step 3: if identical, true
 
 const eqArrays = (arrayOne, arrayTwo) => {
-
   if (arrayOne.length !== arrayTwo.length) {
     return false;
-  } 
+  }
 
   for (let i = 0; i < arrayOne.length; i++) {
     if (arrayOne[i] !== arrayTwo[i]) {
       return false;
     }
-  } 
+  }
   return true;
-}
+};
 
 assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
 assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), true);
-assertEqual(eqArrays(['a', 'b', 'c'], ['a', 'b', 'c']), true);
-assertEqual(eqArrays(['a', 'b', 'c'], ['c', 'b', 'a']), true);
-
+assertEqual(eqArrays(["a", "b", "c"], ["a", "b", "c"]), true);
+assertEqual(eqArrays(["a", "b", "c"], ["c", "b", "a"]), true);
