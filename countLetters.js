b0a9d12b655874ab07e4e9100049c4676fe5a4e1
Brian Sohn <kolpp15@gmail.com>
2021-07-21 17:29:27 -0700

    counting letters in a sentence

---
+++ countLetters.js
@@ -0,0 +1,34 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅Assertion Passed: ${actual} === ${expected}`);
+  } else if (actual !== expected) {
+    console.log(`🛑🛑🛑Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const countLetters = (sentence) => {
+  let result = {};
+
+  // first get rid of the spaces
+  let separatedSentence = sentence.split(' ').join('');
+
+  // loop through sentence
+  for (const letter of separatedSentence) {
+    // check if letter exists
+    if (result[letter]) {
+      result[letter] += 1;
+    } else {
+      // if letter doesn't exist
+      result[letter] = 1;
+    }
+  }
+  return result;
+};
+
+console.log(countLetters('briannnnnn hi'));
+
+assertEqual(countLetters("briannnnnn")['b'], 1);
+assertEqual(countLetters("briannnnnn")['r'], 1);
+assertEqual(countLetters("briannnnnn")['i'], 1);
+assertEqual(countLetters("briannnnnn")['a'], 1);
+assertEqual(countLetters("briannnnnn")['n'], 6);
